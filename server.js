7708225e0ec7f1c35dd7b319788b6d6eb5248d2f
Dqrkky <staurospapadogiannopoulos@gmail.com>
2026-04-28 22:37:44 +0300

    better version

--- server.js
+++ server.js
@@ -28,20 +28,32 @@ app.get("/dashboard/stats", auth.verify, (req,res)=>res.json(db.getStats()));
 
 // WebSocket
 const wss = new WebSocket.Server({ noServer:true });
-function broadcast(event){
+
+function broadcast(event) {
   wss.clients.forEach(c=>{
-    if(c.readyState===WebSocket.OPEN)c.send(JSON.stringify(event));
+    if(c.readyState===WebSocket.OPEN) c.send(JSON.stringify(event));
   });
 }
 
 // Hook db logger
-const originalLog=db.logEvent;
-db.logEvent=async(event)=>{
-  await originalLog(event);
-  broadcast(event);
-};
+// const originalLog=db.logEvent;
+// db.logEvent = async(event) => {
+//   await originalLog(event);
+//   broadcast(event);
+// };
+
+function withBroadcast(original) {
+  return async (event) => {
+    await original(event);
+    broadcast(event);
+  };
+}
+
+db.logEvent = withBroadcast(db.logEvent);
 
 const server = app.listen(3000,()=>console.log("Server running on http://localhost:3000"));
-server.on("upgrade",(req,socket,head)=>{
-  if(req.url==="/ws") wss.handleUpgrade(req,socket,head,ws=>wss.emit("connection",ws,req));
+server.on("upgrade", (req, socket, head) => {
+  if(req.url==="/ws") wss.handleUpgrade(req, socket, head, ws =>
+    wss.emit("connection", ws, req)
+  );
 });
